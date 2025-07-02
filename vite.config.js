27187993cb313c69a5f09df481e0c69d727a1da1
Andres Sarmiento <asarmientosuarez@gmail.com>
2025-07-01 20:43:59 -0400

    vite.config.js corregido allowedhosts

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: 'all',
-  },
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '192.168.0.8',
+      'cecf-189-28-67-179.ngrok-free.app',
+      '54c8-189-28-67-179.ngrok-free.app',
+    ]
+  }
 })
