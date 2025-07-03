74b186dbd54a3c03695297fb1e41b45c6df05844
Andres Sarmiento <asarmientosuarez@gmail.com>
2025-07-02 21:15:22 -0400

    cambios en la version español: banderas incluidas, details eliminado, paginas en ingles creadas

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 import './sass/styles.scss';
 
-const nav = document.querySelector("#nav_bar");
-const abrir = document.querySelector("#menu_button");
-const cerrar = document.querySelector("#close_button");
-
-abrir.addEventListener("click", () =>{
-    nav.classList.add("visible");
-});
-
-cerrar.addEventListener("click", () =>{
-    nav.classList.remove("visible");
-})
