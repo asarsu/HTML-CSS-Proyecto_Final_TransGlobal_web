e07fbf1a5d3b9405c2cbce848d5206eefe2f0092
Andres Sarmiento <asarmientosuarez@gmail.com>
2025-08-05 19:04:17 -0400

    fichero public creado, rutas corregidas, build listo con fichero dist creado y listo para despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
@@ -11,5 +12,23 @@ export default defineConfig({
       'cecf-189-28-67-179.ngrok-free.app',
       '54c8-189-28-67-179.ngrok-free.app',
     ]
+  },
+
+  base: '/HTML-CSS-Proyecto_Final_TransGlobal_web/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nosotros: resolve(__dirname, 'nosotros.html'),
+        servicios: resolve(__dirname, 'servicios.html'),
+        contacto: resolve(__dirname, 'contacto.html'),
+        home: resolve(__dirname, 'home.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    }
+  
   }
 })
