7e40a98a5647790d008db56c237c6ee8214f8713
Andres Sarmiento <asarmientosuarez@gmail.com>
2025-08-19 10:45:42 -0400

    rollUp thanks corregido

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         services: resolve(__dirname, 'services.html'),
         contact: resolve(__dirname, 'contact.html'),
         gracias: resolve(__dirname, 'gracias.html'),
-        gracias: resolve(__dirname, 'thanks.html'),
+        thanks: resolve(__dirname, 'thanks.html'),
       },
     }
   
